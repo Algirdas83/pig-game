e3d4c863820a1b4e7d22aff47620b3a00d800603
Algirdas83 <algirdasliutkus@gmail.com>
2022-11-03 18:26:08 +0200

    dice changing  adding curent score

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 'use strict';
-const btnRollDice = document.querySelector('.btn--roll');
+//Selecting elemnts
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const dice = document.querySelector('.dice');
-let curentScore = 0;
-let totalScore = 0;
-let score = document.querySelector('.score');
-let current = document.querySelector('#current--0');
-///////////////
 
-btnRollDice.addEventListener('click', function () {
-  let radomRollNumber = Math.floor(Math.random() * 6 + 1);
-  if (radomRollNumber !== 1) {
-    curentScore += radomRollNumber;
-    current.textContent = curentScore;
+// Starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+// The Roling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1.Generaiting radom dice roll
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  //2. Dispaly dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3.Check for rolled 1
+  if (dice !== 1) {
+    //Add dice to curent score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later
   } else {
-    curentScore = 0;
-    current.textContent = curentScore;
+    //switch to next player
   }
-  dice.setAttribute(`src`, `dice-${radomRollNumber}.png`);
-});
-
-btnHold.addEventListener('click', function () {
-  score.textContent = totalScore + curentScore;
 });
